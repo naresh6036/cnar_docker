97ada8a01c573c748abd591bb929aaa820aff35d
cnar <naresh6036@gmail.com>
2020-05-19 18:17:12 +0530

    added new projet

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from redis import Redis
+import os
+
+app = Flask(__name__)
+
+#redis = Redis(host='redis', port=6379)
+
+@app.route('/')
+def hello():
+    #count = redis.incr('hits')
+    return '<h1 style="color:red">Welcome to python app</h1>'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
